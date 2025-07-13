da6f376711e19ea54bc57f0753db4869f75ef5a1
mahmoudmansour <mahmoudmansooour@gmail.com>
2025-07-13 16:18:52 +0300

    BlogCategory crud

--- app/Http/Controllers/API/BlogCategoryController.php
+++ app/Http/Controllers/API/BlogCategoryController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\BlogCategory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Str;
 
 class BlogCategoryController extends Controller
 {
@@ -12,7 +15,12 @@ class BlogCategoryController extends Controller
      */
     public function index()
     {
-        //
+        $data = BlogCategory::get();
+        return response()->json([
+            'status' => 'success' ,
+            'count' => count($data),
+            'data' => $data
+        ] , 200);
     }
 
     /**
@@ -20,7 +28,25 @@ class BlogCategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all() , [
+            'name' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'fail' ,
+                'message' => $validator->errors()
+            ] , 400);
+        }
+
+        $data['name'] = $request->name;
+        $data['slug'] = Str::slug($request->name);
+        BlogCategory::create($data);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'category created successfuly'
+        ] , 201);
     }
 
     /**
@@ -36,7 +62,34 @@ class BlogCategoryController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validator = Validator::make($request->all() , [
+            'name' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'fail' ,
+                'message' => $validator->errors()
+            ] , 400);
+
+        }
+
+        $category = BlogCategory::find($id);
+        if(!$category){
+            return response()->json([
+                'status' => 'fail',
+                'message' => 'no category found'
+            ], 404);
+
+        }
+
+        $category->name = $request->name;
+        $category->slug = Str::slug($request->name);
+        $category->save();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'data updated successfuly'
+        ] , 201);
     }
 
     /**
@@ -44,6 +97,18 @@ class BlogCategoryController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $category = BlogCategory::find($id);
+        if (!$category) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => 'No Category found',
+            ] , 404);
+        }
+
+        $category->delete();
+        return response()->json([
+            'status' => 'success' ,
+            'message' => 'category deleted successfuly'
+        ] , 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\BlogCategoryController;
 use App\Http\Controllers\API\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,5 +17,9 @@ Route::apiResource('/students' , StudentController::class);
 
 Route::group(["middleware" => "auth:sanctum"] , function () {
     Route::get('/profile' , [AuthController::Class , "profile"])->name('profile');
-    Route::get("logout" , [AuthController::class , "logout"])->name("logout");
+    Route::get("/logout" , [AuthController::class , "logout"])->name("logout");
+
+    Route::apiResource('/categories' , BlogCategoryController::class);
 });
+
+Route::get('/categories' , [BlogCategoryController::class , 'index']);
